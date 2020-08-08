0e9656c82faba34c9d8c474cb32c2aff126cee9e
bhalani97 <bhalanidivyesh@gmailcom>
2020-08-08 15:58:13 +0530

    sc

--- config/security.js
+++ config/security.js
@@ -30,7 +30,7 @@ module.exports.security = {
 
   cors: {
     allRoutes: true,
-    allowOrigins: '*',
+    allowOrigins: ['http://localhost','https://expensetrackerbyd.herokuapp.com'],
     allowCredentials: false,
     allowRequestHeaders: 'content-type,accept,authorization',
     allowRequestMethods: 'GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD',
